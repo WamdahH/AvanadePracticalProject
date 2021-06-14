79d20efa0ecb79be08c3fba943a1da4dfa781541
Wamdah Hassan <wamdah.hassan@avanade.com>
2021-06-14 17:26:07 +0100

    Letters and numbers

---
+++ code/Merge/Controllers/MergeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Merge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        //numbers: https://localhost:44361/
+        //letters : https://localhost:44321/
+    }
+}
---
+++ code/RandLetters/Controllers/RandLettersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RandLetters.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandLettersController : ControllerBase
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            // random lowercase letter
+            int a = random.Next(0, 26);
+            char ch = (char)('a' + a);
+            Console.WriteLine(ch);
+        }
+    }
+}
